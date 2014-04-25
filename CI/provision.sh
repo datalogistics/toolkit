70a1782c7dda7c69b02b14d9d110dded919683fb
Andrew Melo <andrew.m.melo@vanderbilt.edu>
2014-04-25 18:18:28 -0500

    updating provisioning script

--- CI/provision.sh
+++ CI/provision.sh
@@ -4,19 +4,19 @@ curl -o epel.rpm http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.
 rpm -Uvh epel.rpm 
 rpm --import http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key
 JENKINS_MASTER_DEPS="docker-io java-1.7.0-openjdk jenkins nano"
-DLT_DEVELOPMENT_DEPS="git gcc libcurl-devel jansson-devel autoconf213 openssl-devel rpm-build rpmlint rpmdevtools make"
+DLT_DEVELOPMENT_DEPS="git gcc libcurl-devel jansson-devel autoconf213 openssl-devel rpm-build rpmlint rpmdevtools make cmake28 mock"
 yum -y install ${JENKINS_MASTER_DEPS} ${DLT_DEVELOPMENT_DEPS}
+newgrp mock && usermod -a -G mock vagrant && newgrp mock
 rm -rf /var/lib/jenkins
 git clone https://github.com/datalogistics/CI.git /var/lib/jenkins
 mkdir /var/lib/jenkins/.ssh
-cp /vagrant/id_rsa* /var/lib/jenkins/.ssh
+cp /vagrant/id_rsa* /var/lib/jenkins/.ssh || true
 chown -R jenkins:jenkins /var/lib/jenkins
 chmod 700 /var/lib/jenkins/.ssh
 chmod 600 /var/lib/jenkins/.ssh/*
 sudo -u jenkins ssh-keyscan -H github.com >> /var/lib/jenkins/.ssh/known_hosts
 chown jenkins:jenkins /var/lib/jenkins/.ssh/known_hosts
 chmod 600 /var/lib/jenkins/.ssh/known_hosts
-cat /var/lib/jenkins/.ssh/known_hosts
 service jenkins start
 chkconfig jenkins on
 service docker start
