2a75239f4b8b51c64b57204e3011eb6501afc4bf
Amey Jahagirdar <ajahagir@indiana.edu>
2014-05-07 20:20:37 -0400

    changed to point to dlt.org/yum

--- vagrant-devbox/vagrant-centos-rpm-test/bootstrap.sh
+++ vagrant-devbox/vagrant-centos-rpm-test/bootstrap.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-yum-config-manager --add-repo http://damsl.cs.indiana.edu/yum/damsl-centos-6.repo
-rpm --import http://damsl.cs.indiana.edu/yum/RPM-GPG-KEY-damsl-6
+yum-config-manager --add-repo http://www.data-logistics.org/yum/damsl-centos-6.repo
+rpm --import http://www.data-logistics.org/yum/RPM-GPG-KEY-damsl-6
+rpm --import http://www.data-logistics.org/yum/RPM-GPG-KEY-EPEL-6
 yum check-update
 yum -y install dlt xsp phoebus
