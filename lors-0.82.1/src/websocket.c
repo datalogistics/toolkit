b79374290b5cb9ec0bfebc46055c56386e140532
Akshay <adorwat@indian.edu>
2015-03-15 18:11:48 +0000

    more error handling

--- lors-0.82.1/src/websocket.c
+++ lors-0.82.1/src/websocket.c
@@ -58,7 +58,7 @@ int callback_socket_io(struct libwebsocket_context *context,
 			}
 		}
 		if((time(NULL) - handle->last_ping) > PING_INTERVAL){
-			fprintf(stderr, "Sending Ping packet\n");
+			//fprintf(stderr, "Sending Ping packet\n");
 			write_buff = malloc(strlen(ping_msg) + LWS_SEND_BUFFER_PRE_PADDING + LWS_SEND_BUFFER_POST_PADDING);
 			memcpy((write_buff + LWS_SEND_BUFFER_PRE_PADDING), ping_msg, strlen(ping_msg));
 			libwebsocket_write(wsi, (write_buff + LWS_SEND_BUFFER_PRE_PADDING), strlen(ping_msg), LWS_WRITE_TEXT);
