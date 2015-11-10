295ad8b8fbfc139f8eee5cb2d1b3ea4df20f8e6c
jayaajay <jayaajay@indiana.edu>
2015-11-10 01:19:06 -0500

    Moved to dlt-lors repo

--- lors-0.82.1/lbone/ibp/acconfig.h
+++
@@ -1,8 +0,0 @@
-#undef socklen_t
-#undef ulong_t
-#undef ushort_t
-#undef uint64_t
-#undef int64_t
-#undef u_int64_t
-#undef HAVE_INT64_T
-#undef IBP_DEBUG
--- lors-0.82.1/lbone/ibp/include/ibp.h
+++
@@ -1,9 +0,0 @@
-#ifndef _IBP_H
-#define _IBP_H
-
-#include "ibp/ibp_os.h"
-#include "ibp/ibp_protocol.h"
-#include "ibp/ibp_ClientLib.h"
-#include "ibp/ibp_errno.h"
-
-#endif
